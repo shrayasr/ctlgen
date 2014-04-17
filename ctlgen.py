f1a343bfdf706891635c4270930b0384eacd58c6
Shrayas <shrayasr@gmail.com>
2014-04-17 14:02:59 +0530

    Finishing first version.
    Can write to file also

--- ctlgen.py
+++ ctlgen.py
@@ -1,7 +1,7 @@
 """
 
 Usage:
-    ctlgen --schema <schema_name> --table <table_name> --csv <csv_file> --record <record_delimiter> --field <field_delimiter> --error <error_log>
+    ctlgen --schema <schema_name> --table <table_name> --csv <csv_file> --record <record_delimiter> --field <field_delimiter> --error <error_log> --output_file <output_file>
 
 Arguments:
     schema_name name of the schema to insert the CSV into
@@ -10,6 +10,8 @@ Arguments:
     record_delimiter character to delimit each record by
     field_delimiter character to delimit each field by
     error_log the file to log errors to
+    output_file the file to write the ctl info into
+
 
 Options:
     -h --help Display all the options.
@@ -17,6 +19,18 @@ Options:
 
 import sys
 from docopt import docopt
+
+def create_ctl_file(schema_name, table_name, csv_file, record_delimiter, 
+        field_delimiter, error_log):
+
+    return """
+    import data
+    into table %s."%s"
+    from '%s'
+    record delimited by '%s'
+    fields delimited by '%s'
+    error log '%s'
+    """ % (schema_name, table_name, csv_file, record_delimiter, field_delimiter, error_log)
 
 def main():
 
@@ -28,7 +42,13 @@ def main():
     record_delimiter = arguments.get("<record_delimiter>")
     field_delimiter = arguments.get("<field_delimiter>")
     error_log = arguments.get("<error_log>")
+    output_file = arguments.get("<output_file>")
 
+    ctl_contents = create_ctl_file(schema_name, table_name, csv_file, 
+            record_delimiter, field_delimiter, error_log)
+
+    with open(output_file, 'w') as f:
+        f.write(ctl_contents)
 
 if __name__ == "__main__":
     main()
